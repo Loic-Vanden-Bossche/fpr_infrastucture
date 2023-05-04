6606508da31cd6b73e6b4024edb4d0044bf5539a
Loïc Vanden Bossche <loloic066@gmail.com>
2023-05-04 06:48:24 +0200

    Cloudfront http3

--- cloudfront.tf
+++ cloudfront.tf
@@ -6,40 +6,50 @@ resource "aws_cloudfront_distribution" "cf_dist" {
   enabled             = true
   aliases             = [var.domain_name]
   default_root_object = "index.html"
+
   origin {
     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.bucket.id
+
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
   }
+
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
     target_origin_id       = aws_s3_bucket.bucket.id
-    viewer_protocol_policy = "redirect-to-https" # other options - https only, http
+    viewer_protocol_policy = "redirect-to-https"
+
     forwarded_values {
       headers      = []
       query_string = true
+
       cookies {
         forward = "all"
       }
     }
   }
+
+  http_version = "http2and3"
+
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
       locations        = ["FR"]
     }
   }
+
   tags = {
     "Project"   = var.domain_name
     "ManagedBy" = "Terraform"
   }
+
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.public-cert-fronted.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 
   depends_on = [aws_acm_certificate_validation.fronted]
