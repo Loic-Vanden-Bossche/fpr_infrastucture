61527e3cce51616a901f8cfb03cc003b1c5aff65
Loïc Vanden Bossche <loloic066@gmail.com>
2023-06-05 01:38:40 +0200

    updated service_security_group

--- security-groups.tf
+++ security-groups.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "load_balancer_security_group" {
 
 resource "aws_security_group" "service_security_group" {
   ingress {
+    from_port   = 8070
+    to_port     = 8070
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
