14c0daf238e72e1c9e54e09461cd08f739782042
Loïc Vanden Bossche <loloic066@gmail.com>
2023-06-21 16:50:09 +0200

    Added ecr repository for executor/builder

--- erc.tf
+++ erc.tf
@@ -8,3 +8,14 @@ resource "aws_ecr_repository" "fpr_games_repository" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_repository" "fpr_executor_repository" {
+  name                 = "fpr-executor-repository"
+  image_tag_mutability = "MUTABLE"
+
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
