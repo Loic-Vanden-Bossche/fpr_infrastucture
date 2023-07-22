a30110f909b3bf36eb1607f0cefe5949bd3c988e
Loïc Vanden Bossche <loloic066@gmail.com>
2023-07-22 02:08:53 +0200

    Added extra ports to turn config

--- ec2-turn.tf
+++ ec2-turn.tf
@@ -66,4 +66,12 @@ resource "aws_security_group" "turn-server_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    description = "HTTP Port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
